b8f5142ebdba6af2a1995968d022a9f17043d4b0
MrZeLee <joseluismoura98@gmail.com>
2024-09-06 10:30:01 +0100

    fix: go around the nix zsh

--- flake.nix
+++ flake.nix
@@ -57,7 +57,11 @@
 
             # Create /etc/zshrc that loads the nix-darwin environment.
             # this is required if you want to use darwin's default shell - zsh
-            programs.zsh.enable = true;
+            # programs.zsh = {
+            #     enable = true;
+            #     enableCompletion = false;
+            #     enableGlobalCompInit = false;
+            # };
             environment.shells = [
                 pkgs.zsh
             ];
