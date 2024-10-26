fcf9f085c0bda15e9644f55c0b460e41980248df
MrZeLee <joseluismoura98@gmail.com>
2024-10-26 20:45:48 +0200

    feat(zsh): add custom function for yanking text to clipboard in visual mode feat(aerospace.toml): add new key bindings for app mode to launch applications quickly

--- .bindkey.zsh
+++ .bindkey.zsh
@@ -1,3 +1,10 @@
 # Where should I put you?
 bindkey -s ^f "tmux-sessionizer\n"
 bindkey -s ^h "cd ~\n"
+
+function zvm_vi_yank() {
+	zvm_yank
+	echo -n ${CUTBUFFER} | pbcopy
+	zvm_exit_visual_mode
+}
+
