e73a16fa8214d6147cf4f9f4da64e59d64c40570
MrZeLee <mrzelee404@gmail.com>
2025-01-21 17:33:28 +0000

    feat(configuration.nix): add libnotify to the desktop environment for enhanced notification support

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -123,6 +123,7 @@
     slurp # screenshot functionality
     wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
     mako # notification system developed by swaywm maintainer
+    libnotify
     swaylock
     swayidle
     swaybg
