9dba64b390a66b7fd20df21104eb0cbbd1fcf9d8
MrZeLee <mrzelee404@gmail.com>
2025-03-03 12:38:55 +0000

    feat(focus.lua): set width for autoresize option in focus plugin

--- .config/nvim/lua/mrzelee/plugins/focus.lua
+++ .config/nvim/lua/mrzelee/plugins/focus.lua
@@ -4,6 +4,7 @@ return {
 	config = function()
 		require("focus").setup({
 			autoresize = {
+        width = 90,
 				minwidth = 3, -- Force minimum width for the unfocused window
 				minheight = 3, -- Force minimum height for the unfocused window
 			},
