a937b32fa244e75b975a79982ea0fd4581b08757
MrZeLee <mrzelee404@gmail.com>
2025-03-04 15:55:46 +0000

    feat: update focus.nvim plugin to MrZeLee/focus.nvim repository

--- .config/nvim/lua/mrzelee/plugins/focus.lua
+++ .config/nvim/lua/mrzelee/plugins/focus.lua
@@ -1,11 +1,16 @@
 return {
-	"nvim-focus/focus.nvim",
+  -- "nvim-focus/focus.nvim",
+	"MrZeLee/focus.nvim",
+  branch = "dev",
+  -- dir = "/home/mrzelee/Documents/01-Git/focus.nvim/",
+  -- dev = true,
 	version = false,
 	config = function()
 		require("focus").setup({
 			autoresize = {
 				minwidth = 3, -- Force minimum width for the unfocused window
 				minheight = 3, -- Force minimum height for the unfocused window
+        focusedwindow_minwidth = 90,
 			},
 		})
 	end,
