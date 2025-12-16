538cf587aa1dc67695588cead6d889758152138e
MrZeLee <mrzelee@proton.me>
2025-12-16 10:23:05 +0000

    feat(config): add gtk-3.0 settings and neovim keymap for tmux-sessionizer
    This commit introduces a new GTK settings file to prefer dark themes and adds a Neovim keymap for launching the tmux-sessionizer. It also updates the tmux configuration to use a generic 'copy' command instead of 'pbcopy' for broader compatibility.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set(
+  { "n", "t", "i" },
+  "<C-F>",
+  "<cmd>silent !tmux neww -n 'sessionizer' bash -c 'tmux-sessionizer'<CR>",
+  { desc = "Open Tmux Sessionizer" }
+)
