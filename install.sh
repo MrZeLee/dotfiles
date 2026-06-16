45d38ee391c3f6f5f76b5b2f45fabe434b157f36
MrZeLee <mrzelee@proton.me>
2026-06-16 15:52:07 +0100

    feat(gpg): Introduce flexible pinentry wrapper and templated config

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ OS_TYPE=$(uname)
 echo "Operating System: $OS_TYPE"
 
 # Check if Nix is already installed
-if ! command -v nix &> /dev/null; then
+if ! command -v nix &>/dev/null; then
   echo "Nix is not installed. Installing Nix..."
   # Install Nix
   curl -L https://nixos.org/nix/install | sh
@@ -16,7 +16,7 @@ fi
 # If the operating system is macOS
 if [ "$OS_TYPE" = "Darwin" ]; then
   # Check if Homebrew is installed
-  if ! command -v brew &> /dev/null; then
+  if ! command -v brew &>/dev/null; then
     echo "Homebrew is not installed. Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   else
@@ -25,7 +25,7 @@ if [ "$OS_TYPE" = "Darwin" ]; then
 fi
 
 # Check if Git is installed; if not, install it using Nix
-if ! command -v git &> /dev/null; then
+if ! command -v git &>/dev/null; then
   echo "Git is not installed. Installing Git with Nix..."
   nix-env -iA nixpkgs.git
 else
@@ -42,7 +42,7 @@ else
 fi
 
 # Check if stow is installed; if not, install it using Nix
-if ! command -v stow &> /dev/null; then
+if ! command -v stow &>/dev/null; then
   echo "Stow is not installed. Installing Stow with Nix..."
   nix-env -iA nixpkgs.stow
 else
@@ -60,7 +60,7 @@ if [ "$OS_TYPE" = "Darwin" ]; then
   DOTFILES_PLIST="$HOME/.dotfiles/com.apple.HIToolbox.plist"
   PREFERENCES_PLIST="$HOME/Library/Preferences/com.apple.HIToolbox.plist"
 
-  if sudo diff "$DOTFILES_PLIST" "$PREFERENCES_PLIST" > /dev/null 2>&1; then
+  if sudo diff "$DOTFILES_PLIST" "$PREFERENCES_PLIST" >/dev/null 2>&1; then
     echo "No differences found between the plist files."
   else
     echo "Differences found between the plist files. Copying..."
@@ -69,8 +69,10 @@ if [ "$OS_TYPE" = "Darwin" ]; then
   fi
 fi
 
-mkdir $HOME/.local 2>/dev/null
-mkdir $HOME/.local/bin/ 2>/dev/null
-ln -s $HOME/.config/tmux/scripts/tmux-sessionizer $HOME/.local/bin/
-ln -s $HOME/.config/tmux/scripts/tmux-windowizer $HOME/.local/bin/
+mkdir "$HOME/.local" 2>/dev/null
+mkdir "$HOME/.local/bin/" 2>/dev/null
+ln -s "$HOME/.config/tmux/scripts/tmux-sessionizer" "$HOME/.local/bin/"
+ln -s "$HOME/.config/tmux/scripts/tmux-windowizer" "$HOME/.local/bin/"
 
+chmod 700 "$HOME/.dotfiles/.gnupg"
+sed "s|\$HOME|$HOME|g" "$HOME/.dotfiles/.gnupg/gpg-agent.conf.template" >"$HOME/.dotfiles/.gnupg/gpg-agent.conf"
