b3e01558e1f919c111c0dbea38bfb7bcc1275694
MrZeLee <mrzelee@proton.me>
2026-06-16 15:06:23 +0100

    feat(dev-env): Configure AI assistant and editor tooling

--- .config/nvim/lua/plugins/tuxedo.lua
+++ .config/nvim/lua/plugins/tuxedo.lua
@@ -2,6 +2,9 @@
 return {
   "IogaMaster/tuxedo.nvim",
   cmd = "Tuxedo",
+  keys = {
+    { "<leader>td", "<cmd>Tuxedo<cr>", desc = "Open Tuxedo (TODO)" },
+  },
   opts = {
     create_todo_file = true,
     width_ratio = 0.95,
